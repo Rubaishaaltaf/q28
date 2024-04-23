1e4003fd7afaec29f8ab719534a0559705cd3773
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-04-23 14:04:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// Creating a variable
+var age = 100;
+// Creating a program for Determining the stage of Life Using  If-Else Chain
+if (age < 2) {
+    console.log("You are a Baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("You are Toddler");
+}
+else if (age >= 4 && age < 13) {
+    console.log("You are a Kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("You are a Teenagers.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("you are a Adults.");
+}
+else if (age >= 65) {
+    console.log("you are Elders.");
+}
